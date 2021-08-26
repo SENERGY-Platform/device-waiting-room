baea03b09d23d3dc0f45b2cf97db755fa05663b8
Ingo <roessner@infai.org>
2021-08-26 09:15:25 +0200

    remove unused config field

--- pkg/configuration/config.go
+++ pkg/configuration/config.go
@@ -14,7 +14,6 @@ import (
 type Config struct {
 	ApiPort                    string `json:"api_port"`
 	MongoUrl                   string `json:"mongo_url"`
-	MongoReplSet               bool   `json:"mongo_repl_set"` //set true if mongodb is configured as replication set or mongos and is able to handle transactions
 	MongoTable                 string `json:"mongo_table"`
 	MongoDeviceCollection      string `json:"mongo_device_collection"`
 	Debug                      bool   `json:"debug"`
